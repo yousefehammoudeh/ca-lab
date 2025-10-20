8ab64036eccbcb2f6f8fe8fc6e77b5f7107e294b
He Sun <sh.sun@mail.utoronto.ca>
2025-10-20 09:41:53 -0400

    Implemented execute()

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -15,10 +15,9 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     @Override
     public void execute() {
-        // TODO: implement the logic of the Logout Use Case
-        // * set the current username to null in the DAO
-        // * instantiate the `LogoutOutputData`, which needs to contain the username.
-        // * tell the presenter to prepare a success view.
+        String username = userDataAccessObject.getCurrentUsername();
+        userDataAccessObject.setCurrentUsername(null);
+        LogoutOutputData outputData = new LogoutOutputData(username);
+        logoutPresenter.prepareSuccessView(outputData);
     }
 }
-
